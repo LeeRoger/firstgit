31fa9f95d30c01a6cb7884cb51e7474f1b921bac
LeeRoger <307539843@qq.com>
2018-01-08 22:20:10 +0800

    更新任务二页面的header，新增洗牌方法，待完成洗牌任务

--- js/task02/task02.js
+++ js/task02/task02.js
@@ -1,30 +1,31 @@
 var roleNumNodeList = document.getElementsByClassName("player-detail-role-num");
-var roleNum = {
-    killer: 0,
-    police: 0,
-    civilian: 0,
-    judge: 1
-};
+var roleNum = { killer: 0, police: 0, civilian: 0, judge: 1 };
 var roleNumList = [roleNum.killer, roleNum.police, roleNum.civilian, roleNum.judge];
+var playerRoleList = new Array(playerNum);
+var playerRoleListShuffled = new Array(playerNum);
 
-var popErrNum = document.getElementById("popErrNum");
-//var playerNumInput = player-num-set.player-num-input;
+// 获取input.value，需注意命名规范，下面三种方式都可以，当然还有更多写法
+//var playerNumInput = playerNumSet.playerNumInput; // player-num-set.player-num-input或者player_num_set.player_num_input都是错误的
 //var playerNumInput = document.forms[0].elements["player-num-input"];
 var playerNumInput = document.getElementById("playerNumInput");
 var playerNumSetBar = document.getElementById("playerNumSetBar");
-//var playerNum = (playerNumInput.value==playerNumSetbar.value) ? playerNumInput.value : playerNumSetBar.value;
-var playerNum;
+var playerNum = 0;
 
-var choiceConfirm = document.getElementById("popErrNumChoiceConfirm");
 var popErr = document.getElementById("popErr");
+var popErrNum = document.getElementById("popErrNum");
+var choiceConfirm = document.getElementById("popErrNumChoiceConfirm");
 var gameStart = document.getElementById("gameStart");
 
-/*popErrNum.onclick = function() {
-    popErrNum.style.transform = "translate(-50%, 0)";
-    setTimeout(function() {
-        popErrNum.style.transform = "translate(-50%, -50%)";
-    }, 2000);
-}*/
+// 洗牌算法
+function shuffle(array) {
+    var _array = array.concat();
+    for (var i=_array.length; i>=0; i--) {
+        var j = Math.floor(Math.random() * (i+1));  // 0<=j<=i
+        var temp = _array[i];
+        _array[i] = _array[j];
+        _array[j] = temp;
+    }
+}
 
 // 获得玩家数量
 function getPlayerNum() {
@@ -37,18 +38,14 @@ function updateRoleNumList(roleNum) {
 }
 
 // 获得角色数量
-function getRoleNum(num) {
-    if ((num-1)%4 == 3) {
-        roleNum.killer = Math.ceil((num-1)/4);
-    } else {
-        roleNum.killer = Math.floor((num-1)/4);
-    }
-    console.log(roleNum.killer + '人' + ' FgetRoleNum');
+function getRoleNum(playerNum) {
+    roleNum.killer = ((playerNum-1)%4 == 3) ? Math.ceil((playerNum-1)/4) : Math.floor((playerNum-1)/4);
+    console.log(roleNum.killer + '人' + ' FgetRoleNum.roleNum.killer');
     roleNum.police = roleNum.killer;
-    roleNum.civilian = num - 1 - 2 * roleNum.killer;
-    console.log(roleNumList + ' FgetRoleNum.updatebefore');
+    roleNum.civilian = playerNum - 1 - 2 * roleNum.killer;
+    console.log(roleNumList + ' FgetRoleNum.roleNumList.updatebefore');
     updateRoleNumList(roleNum);
-    console.log(roleNumList + ' FgetRoleNum.updateafter');
+    console.log(roleNumList + ' FgetRoleNum.roleNumList.updateafter');
 }
 
 // 写入角色数量
@@ -58,38 +55,25 @@ function writeRoleNum() {
     }
 }
 
-// 检查输入的值，错误则弹窗
+// 检查输入的值
 function checkInputNum(playerNum) {
-    if (playerNum >= 4 && playerNum <= 18) {
-        return true;
-    } else {
-        popErr.classList.add("is-fixed");
-        return false;
-    }
+    return (playerNum >= 4 && playerNum <= 18) ? true : false;
 }
 
-// 同步两个input的value 
+// 同步两个input的value，同时保证input的值不会错
 function getSameNum() {
     playerNumInput.value = playerNumSetBar.value;
 }
 
-// 同步两个input的value 
-/*
-var getSameNum = function() {
-    playerNumInput.value = playerNumSetBar.value;
-    playerNumInput.onchange = function() {
-        playerNumSetBar.value = playerNumInput.value;
-        var colorWidth = ((parseFloat(playerNumSetBar.value)-4)/14)*100;
-        var colorBreak = ((parseFloat(playerNumSetBar.value)-4)/14)*100+0.1;
-        var linearGradient = 'linear-gradient(to right, #FBB435, #FBB435 ' + colorWidth + '%, white ' + colorBreak +'%, white)';
-        playerNumSetBar.style.background = linearGradient;
-    }
-    playerNumSetBar.onchange = function() {
-        playerNumInput.value = playerNumSetBar.value;
-        playerNumInput.placeholder = playerNumInput.value;
-    }
+
+// 弹出错误提醒并添加动画
+function popErrHint() {
+    popErr.classList.add("is-fixed");
+    popErrNum.style.transform = "translate(-50%, -80%)";
+    setTimeout(function() {
+        popErrNum.style.transform = "translate(-50%, -50%)";
+    }, 100);
 }
-*/
 
 // 获取滑动条的背景渐变
 function getBackground() {
@@ -112,9 +96,16 @@ playerNumSetBar.onchange = function() {
 }
 
 playerNumInput.onchange = function() {
-    playerNumSetBar.value = playerNumInput.value;
-    console.log(playerNumSetBar.value + ' playerNumInput.onchange.playerNumSetBar.value');
-    getBackground();
+    getPlayerNum();
+    if (checkInputNum(playerNum)) {
+        playerNumSetBar.value = playerNumInput.value;
+        console.log(playerNumSetBar.value + ' playerNumInput.onchange.playerNumSetBar.value');
+        getBackground();
+        getRoleNum(playerNum);
+        writeRoleNum();
+    } else {
+        popErrHint();
+    }
 }
 
 choiceConfirm.onclick = function() {
@@ -122,20 +113,45 @@ choiceConfirm.onclick = function() {
     getSameNum();
 }
 
-// getSameNum();
-
+// 用户输入后直接点击“去发牌”后做校验
+// 后续这里应该需要添加传输数据的操作
 gameStart.onclick = function() {
     //getSameNum();
     getPlayerNum();
     if (checkInputNum(playerNum)) {
-        console.log(playerNum + ' button.onclick');
-        console.log(roleNumList + ' button.onclick.beforeupdate');
+        console.log(playerNum + ' button.onclick.playerNum');
+        console.log(roleNumList + ' button.onclick.roleNumList.beforeupdate');
         getRoleNum(playerNum);
         writeRoleNum();
     } else {
-        popErrNum.style.transform = "translate(-50%, -80%)";
-        setTimeout(function() {
-            popErrNum.style.transform = "translate(-50%, -50%)";
-        }, 100);
+        popErrHint();
     }
 }
+
+// 废弃的一些东西
+//var playerNum = (playerNumInput.value==playerNumSetbar.value) ? playerNumInput.value : playerNumSetBar.value;
+// getSameNum();
+// 同步两个input的value 
+/*
+var getSameNum = function() {
+    playerNumInput.value = playerNumSetBar.value;
+    playerNumInput.onchange = function() {
+        playerNumSetBar.value = playerNumInput.value;
+        var colorWidth = ((parseFloat(playerNumSetBar.value)-4)/14)*100;
+        var colorBreak = ((parseFloat(playerNumSetBar.value)-4)/14)*100+0.1;
+        var linearGradient = 'linear-gradient(to right, #FBB435, #FBB435 ' + colorWidth + '%, white ' + colorBreak +'%, white)';
+        playerNumSetBar.style.background = linearGradient;
+    }
+    playerNumSetBar.onchange = function() {
+        playerNumInput.value = playerNumSetBar.value;
+        playerNumInput.placeholder = playerNumInput.value;
+    }
+}
+*/
+
+/*popErrNum.onclick = function() {
+    popErrNum.style.transform = "translate(-50%, 0)";
+    setTimeout(function() {
+        popErrNum.style.transform = "translate(-50%, -50%)";
+    }, 2000);
+}*/

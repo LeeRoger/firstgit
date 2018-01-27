5a0a5cf82b894f5e46620a150d4161ef1e0db0af
LeeRoger <307539843@qq.com>
2018-01-27 19:40:07 +0800

    对有限状态机的理解不够，感觉任务4就是个伪状态机，因为会跳转到其他页面

--- js/task02/task04-judgeFlow.js
+++ js/task02/task04-judgeFlow.js
@@ -1,6 +1,6 @@
 var playersJson = window.localStorage.playersJson;
 var players = jQuery.parseJSON(playersJson);
-var nextUrl = task13-vote.html;
+var nextUrl = "task13-vote.html";
 var day = window.localStorage.day ? window.localStorage.day : "1";
 
 // 获取玩家对象
@@ -15,12 +15,37 @@ function getPlayers() {
     }
 }
 
-// 更新弹出框内容以及即将跳转的带参数的链接
-function updatePopErr() {
-    //var states = ["start", "killer", "police", "sniper", "doctor", "voter"];
+// 获取当前所处状态
+function getState() {
     var stateUrl = window.location.search;
     var patt = /state=(\w+)/;
     var state = stateUrl.match(patt)[1];
+    return state;
+}
+
+// 更新页面天数、状态变化时页面内容的变化
+function updateGameStep() {
+    var addGameStep = $(".game-step:first").clone();
+    for (var i=1; i<day; i++) {
+        $("#game").append(addGameStep);
+    }
+    for (var i=0; i<day-1; i++) {
+        $(".game-step-detail").get(i).classList.add("is-hidden");
+        $(".game-step-hidden").get(i).classList.remove("is-hidden");
+    }
+    var state = getState();
+    var states = ["start", "killer", "police", "sniper", "doctor", "start"];
+    var pos = states.indexOf(state);
+    for (var i=0; i<pos; i++) {
+        var selector = "." + states[i+1] + ":last";
+        $(selector).attr("disabled", "disabled");
+    }
+}
+    
+// 更新弹出框内容以及即将跳转的带参数的链接
+function updatePopErr() {
+    //var states = ["start", "killer", "police", "sniper", "doctor", "voter"];
+    var state = getState();
     switch (state) {
         case "start":
             $("#hintWord").text("即将前往杀手杀人页面");
@@ -47,10 +72,16 @@ function updatePopErr() {
     }
 }
 
+$(document).ready(function() {
+    console.log("123");
+    updateGameStep();
+    updatePopErr();
+});
 
-var fsm = StateMachine.creat({
-    initial: 'start',
-    events: [
+
+var fsm = new StateMachine({
+    init: 'start',
+    transitions: [
         { name: 'kill',  from: 'start',  to: 'killer' },
         { name: 'check', from: 'killer', to: 'police' },
         { name: 'snipe', from: 'police', to: 'sniper' },
@@ -58,7 +89,37 @@ var fsm = StateMachine.creat({
         { name: 'vote',  from: 'doctor', to: 'voter'  },  // 这个好像没有必要了
         { name: 'clear', from: ['killer', 'police', 'sniper', 'doctor', 'voter'], to: 'start'}
     ],
-    methods: [
-        onBeforeEvent
-    ]
+    methods: {
+        onBeforeTranstion: function() {
+            var state = getState();
+            var states = ["start", "killer", "police", "sniper", "doctor", "start"];
+            var pos = states.indexOf(state);
+            var stateClass = "." + states[pos+1];
+            console.log("233");
+            $(".game-step-detail-role").click(function() {
+                if ($(this).hasClass(stateClass)) {
+                    $("#popHint").addClass("is-fixed");
+                    $(".pop-hint-role").animate({transfrom: 'translate(-50%, -70%)'}, "fast");
+                    $(".pop-hint-role").animate({transfrom: 'translate(-50%, -50%)'}, "slow");
+                } else {
+                    $("#hintWord").text("请按照正确的游戏步骤进行哦");
+
+                }
+            });
+        },
+        onkill: function() {
+            $(".killer").one("click", function() {
+                $("#popHint").addClass("is-fixed");
+                $(".pop-hint-role").animate({transfrom: 'translate(-50%, -70%)'}, "fast");
+                $(".pop-hint-role").animate({transfrom: 'translate(-50%, -50%)'}, "slow");
+                $("#roleConfirm").click(function() {
+
+                })
+            })
+        }
+    }
+});
+
+$("killer").click(function() {
+    fsm.kill();
 })
